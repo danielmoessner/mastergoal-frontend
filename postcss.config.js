84206c511c88cc78d0853f98edcc024eda2d3f9d
dmoetech <github@moesibert.de>
2020-09-19 16:31:01 +0200

    removed autoprefixer and cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
-const cssnano = require("cssnano");
-const autoprefixer = require("autoprefixer");
+// const cssnano = require("cssnano");
+// const autoprefixer = require("autoprefixer");
 
 module.exports = ({ env }) => ({
   plugins: [
     require("tailwindcss"),
-    env === "production" ? autoprefixer() : null,
-    env === "production"
-      ? cssnano({
-          preset: "default",
-        })
-      : null,
+    // env === "production" ? autoprefixer() : null,
+    // env === "production"
+    //   ? cssnano({
+    //       preset: "default",
+    //     })
+    //   : null,
   ],
 });
