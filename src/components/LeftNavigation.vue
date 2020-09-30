88986418f2f662211fe237e6564abb50209a040c
dmoetech <github@moesibert.de>
2020-09-30 23:38:23 +0200

    bg color on open

--- src/components/LeftNavigation.vue
+++ src/components/LeftNavigation.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="w-full md:min-h-screen sticky top-0 md:bg-white">
+  <div
+    class="w-full md:min-h-screen sticky top-0 md:bg-white"
+    v-bind:class="{ 'bg-white': open }"
+  >
     <mastergoal-logo v-on:click.native="toggle">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -81,20 +84,15 @@ export default {
     },
   },
   computed: {
-    navigationStyle() {
-      let height = this.navigationHeight;
-      if (this.open) height = 0;
-      return "transform: translateY(-" + height + "px);";
-    },
     changingNavigationHeight() {
-      if (!this.open) return '';
+      if (!this.open) return "";
       return this.navigationHeight + "px";
     },
   },
   data: function() {
     return {
       open: true,
-      navigationHeight: 'auto',
+      navigationHeight: "auto",
       overviewIcon:
         '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
       logoutIcon:
@@ -109,23 +107,11 @@ export default {
     },
     toggle() {
       this.open = !this.open;
-      // this.$refs["navigation"].style.height = height;
-      // this.$refs["navigation"].classList += " collapsing";
     },
   },
   mounted() {
     this.navigationHeight = this.$refs["navigation"].clientHeight;
-    console.log(this.navigationHeight);
     this.open = false;
   },
 };
 </script>
-
-<style>
-.collapsing {
-  position: relative;
-  height: 0;
-  overflow: hidden;
-  transition: height 0.35s ease;
-}
-</style>
