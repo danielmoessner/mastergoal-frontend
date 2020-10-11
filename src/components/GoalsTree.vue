dac771de99ef536205c0126aa1cdeea9af951ed1
dmoetech <github@moesibert.de>
2020-10-11 16:50:24 +0200

    tree

---
+++ src/components/GoalsTree.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full">
+      <tree-item v-for="item in tree" v-bind:key="item.url" v-bind:item="item"></tree-item>
+  </div>
+</template>
+
+<script>
+import NavigationButton from "@/components/NavigationButton.vue";
+import TreeItem from "@/components/TreeItem.vue";
+
+export default {
+  name: "GoalsTree",
+  components: {
+    NavigationButton,
+    TreeItem
+  },
+  props: {
+    tree: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/TreeItem.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div class="w-full flex items-center justify-between">
+      <div class="flex flex-col">
+          <div class="block text-gray-800 leading-tight truncate">{{ item.name }}</div>
+          <div class="block text-xs text-gray-800 leading-tight"></div>
+      </div>
+      <navigation-button link='#' text='Open'></navigation-button>
+  </div>
+  <tree-item v-bind:></tree-item>
+  <tree-item></tree-item>
+  <goals-tree v-bind:tree="item.sub_goals" class="ml-4"></goals-tree>
+  </div>
+</template>
+
+<script>
+import NavigationButton from "@/components/NavigationButton.vue";
+import GoalsTree from "@/components/GoalsTree.vue";
+
+export default {
+  name: "GoalsTree",
+  components: {
+    NavigationButton,
+    GoalsTree
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
