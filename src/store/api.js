4a716ac0506c2c17e10d07e5abf1591020dfcfa3
danielmoessner <github@moesibert.de>
2020-11-21 11:43:14 +0100

    changed api url

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,7 @@ const state = {
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000"
-      : "https://mastergoal.xyz",
+      : "https://goals-api.danielmoessner.de",
 };
 
 export const api = {
