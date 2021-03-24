3932fce5114519ee4a1b20fd6151ba20beef63e1
danielmoessner <github@moesibert.de>
2021-03-24 11:07:04 +0100

    udpastes

--- src/components/MastergoalLogo.vue
+++ src/components/MastergoalLogo.vue
@@ -2,7 +2,7 @@
   <div
     class="flex w-full h-12 px-4 md:justify-center bg-gray-800 rounded-br-lg md:rounded-r-lg items-center justify-between relative"
   >
-    <div class="md:absolute inset-0 w-full h-full flex items-center pl-4">
+    <div class="md:absolute inset-0 w-full h-full flex items-center">
       <span class="text-2xl font-semibold text-gray-100">Mastergoal</span>
     </div>
     <slot></slot>
